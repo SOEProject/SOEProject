84950a5215beb94c48372abb955ce97601d39825
MarcusK <elwi@kth.se>
2015-12-09 09:44:05 +0100

    Puting things in dev in master instead for Jenkins

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:12:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:6:"apiKey";s:128:"b9efa1d3675b20fae89e9f35a1ddfa80891423049dfffbd28310cb36014482f6ccdaad53ce73997b27b76770d6e8a5e55bfdadd685badb59666f7fa76088ca14";s:20:"loginSec_lockoutMins";s:3:"240";s:19:"other_hideWPVersion";s:1:"1";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:26:"loginSec_disableAuthorScan";s:1:"1";s:18:"liveTraf_ignoreIPs";s:0:"";s:17:"liveTraf_ignoreUA";s:0:"";s:6:"encKey";s:16:"7121693d6ab70f82";}
